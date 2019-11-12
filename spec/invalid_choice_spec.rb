40e53339b754f8960d1dfff56e5116268c07aea1
sick-carlito <carlb1319@gmail.com>
2019-11-12 18:38:43 +0300

    added a few more changes

--- spec/invalid_choice_spec.rb
+++ spec/invalid_choice_spec.rb
@@ -5,11 +5,10 @@ require_relative "../bin/main"
 describe ".GameManager" do
   context ' grid ["XOX", "OXO", "XXX"]' do
     let(:output) { double("output").as_null_object }
-    let(:grid) { GameManager.new(output, %w["XOX", "OXO", "XXX"]) }
+    let(:grid) { GameManager.new(output, %w[ XOX OXO XXX ]) }
 
-      context "#valid_move?" do
-        it "X make invalid choice" do
-
+        context "#valid_move?" do
+          it "X make invalid choice" do
           expect(grid.valid_move?(9)).to be false
         end
 
@@ -19,11 +18,11 @@ describe ".GameManager" do
           end
       end
 
-      context "#board_index" do
-        it "should receive invalid choice" do
-          output.expect(:puts).with("invalid choice!")
-          grid.board_index(9)
-        end
+        context "#board_index" do
+          it "should receive invalid choice" do
+            output.expect(:puts).with("invalid choice!")
+            grid.board_index(9)
+          end
 
         it "current player should be X" do
             expect(grid.current_player).to eq(:X)
