f362f308e20d61a6b1153390f2d6abcdac59b2ef
sick-carlito <carlb1319@gmail.com>
2019-11-14 02:32:47 +0300

    Files rearranged for better understanding and tests added

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,24 +1,24 @@
-
+# frozen_string_literal: true
 
 
 class Board
   attr_accessor :game_board, :board_num
   def initialize
-    @game_board = [" "," "," "," "," "," "," "," "," "]
+    @game_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     @board_num = []
   end
 
   def display_board
-    disp =  " #{@game_board[0]} | #{@game_board[1]} | #{@game_board[2]}\n " +
-            "---------\n" +
-            " #{@game_board[3]} | #{@game_board[4]} | #{@game_board[5]}\n " +
-            "---------\n" +
-            " #{@game_board[6]} | #{@game_board[7]} | #{@game_board[8]} "
-    return disp
+    disp =  " #{@game_board[0]}|#{@game_board[1]}|#{@game_board[2]}\n " \
+            "---------\n" \
+            " #{@game_board[3]}|#{@game_board[4]}|#{@game_board[5]}\n " \
+            "---------\n" \
+            " #{@game_board[6]}|#{@game_board[7]}|#{@game_board[8]} "
+  
   end
 
   def add_to_board(choice, player)
-    @game_board[choice-1] = player
+    @game_board[choice - 1] = player
   end
 
   def count_board(player)
@@ -32,8 +32,8 @@ class Board
     end
   end
 
-  def has_winner
-    WIN.any? {|line| (line - @board_num) == [] }
+  def winner?
+    WIN.any? { |line| (line - @board_num) == [] }
   end
 end
 
@@ -41,25 +41,24 @@ class Player
   attr_accessor :choice, :player
   def initialize
     @choice = nil
-    @player = 'X'
+    @player = "X"
   end
 
   def switch
-    if $count % 2 == 0
-      @player = 'X'
+    if ($count % 2).zero?
+      @player = "X"
     else
-      @player = 'O'
+      @player = "O"
     end
   end
 
   def num_choice(game_board)
     @choice = gets.to_i
-    until (game_board[@choice-1] == " ") && (@choice.between?(1,9))
+    until (game_board[@choice - 1] == " ") && @choice.between?(1, 9)
       puts "\nERROR: Number needs to be between 1-9 and not used already...\n"
       puts "Please input another number.\n\n"
       @choice = gets.to_i
     end
-    return @choice
+      @choice
   end
-
 end
